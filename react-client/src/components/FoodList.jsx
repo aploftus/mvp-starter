66071798d0e3977655311e49a405b9feb6a5dd3e
aploftus <aploftus@users.noreply.github.com>
2017-11-11 11:00:14 -0800

    Refactor FoodList

--- react-client/src/components/FoodList.jsx
+++ react-client/src/components/FoodList.jsx
@@ -5,7 +5,12 @@ const FoodList = ({foods}) => (
   <div>
     <h4> Project Component </h4>
     There are { foods.length } items.
-    { foods.map((food, index) => <FoodEntry food={food} key={index}/>)}
+    { foods.map((food, index) => { 
+      return <FoodEntry 
+        food={food}
+        key={index} 
+      /> })
+    }
   </div>
 )
 
