c161537dc31066c8d0adfc5a3a9262f70af6ffbc
aploftus <aploftus@users.noreply.github.com>
2017-11-11 16:52:45 -0800

    Align components

--- react-client/src/components/NutritionInfo.jsx
+++ react-client/src/components/NutritionInfo.jsx
@@ -3,26 +3,28 @@ import NutritionEntry from './NutritionEntry.jsx';
 
 const NutritionInfo = ({nutrients, nutrientCodes, measures, facts}) => (
   <div className="nutrition-info">
-  <img className="food-thumb" src={facts.photo.thumb} />
-    <table>
-      <thead>
-        <tr>
-          <th>Nutrient</th>
-          <th>Amount in serving of {facts.food_name}</th>
-        </tr>
-      </thead>
-      <tbody>
-        { nutrients.map((nutrient, index) => { 
-          return <NutritionEntry 
-            nutrient={nutrient}
-            key={index} 
-            nutrientCode={nutrientCodes[index]}
-            measure={measures[index]}
-            facts={facts.full_nutrients}
-          /> })
-        }
-      </tbody>
-    </table>
+    <img className="food-thumb" src={facts.photo.thumb} />
+    <div className="food-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Nutrient</th>
+            <th>Amount in serving of {facts.food_name}</th>
+          </tr>
+        </thead>
+        <tbody>
+          { nutrients.map((nutrient, index) => { 
+            return <NutritionEntry 
+              nutrient={nutrient}
+              key={index} 
+              nutrientCode={nutrientCodes[index]}
+              measure={measures[index]}
+              facts={facts.full_nutrients}
+            /> })
+          }
+        </tbody>
+      </table>
+    </div>
   </div>
 )
 
