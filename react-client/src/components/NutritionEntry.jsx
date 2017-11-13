aa3945daecaa3b1ad1acb3f8b99399ae630d8d84
aploftus <aploftus@users.noreply.github.com>
2017-11-12 18:27:21 -0800

    Refactor NutritionEntry as class

--- react-client/src/components/NutritionEntry.jsx
+++ react-client/src/components/NutritionEntry.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
-let NutritionEntry = ({nutrient, measure, facts, driTeen, driChild, driAdult}) => (
-  <tr>
-    <td>{nutrient}</td>
-    <td>{facts[nutrient]} {measure}</td>
-    <td>{driChild} {measure}</td>
-    <td>{driTeen} {measure}</td>
-    <td>{driAdult} {measure}</td>
-  </tr>
-)
+class NutritionEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let nutrientAmount = this.props.facts[this.props.nutrient];
+
+    return (
+      <tr>
+        <td>{this.props.nutrient}</td>
+        <td>{nutrientAmount} {this.props.measure}</td>
+        <td>{this.props.driChild} {this.props.measure}</td>
+        <td>{this.props.driTeen} {this.props.measure}</td>
+        <td>{this.props.driAdult} {this.props.measure}</td>
+      </tr>
+    )
+  }
+}
+
+// let NutritionEntry = (props) => (
+//   <tr>
+//     <td>{props.nutrient}</td>
+//     <td>{props.facts[props.nutrient]} {props.measure}</td>
+//     <td>{props.driChild} {props.measure}</td>
+//     <td>{props.driTeen} {props.measure}</td>
+//     <td>{props.driAdult} {props.measure}</td>
+//   </tr>
+// )
+
+
+// {nutrient, measure, facts, driTeen, driChild, driAdult}
 
 export default NutritionEntry;
 
